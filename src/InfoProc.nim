764b3939d66fb09c2e0aa845fd9057c21eeeb3e7
sneekyfoxx <sneekyfoxx09@gmail.com>
2024-01-09 22:42:34 -0500

    Update src/...

--- src/InfoProc.nim
+++ src/InfoProc.nim
@@ -29,23 +29,14 @@ proc info*(entry: string) {.noreturn.} =
     quit(1)
 
   size = "{info.size.float / 1000:2.1f}KB".fmt
-  tmp = expandTilde(entry).split("/")
 
   str.add([pcFile: '-', 'l', pcDir: 'd', 'l'][info.kind])
 
   for pos, perm in [fpUserRead, fpUserWrite, fpUserExec, fpGroupRead, fpGroupWrite, fpGroupExec, fpOthersRead, fpOthersWrite, fpOthersExec]:
     str.add(if perm in info.permissions: "rwx"[pos mod 3] else: '-')
 
-  for val in 0 .. tmp.len:
-    try:
-      if tmp[val] == "" or tmp[val] == " ":
-        tmp.delete(val)
-
-    except IndexDefect:
-      break
-
   stdout.writeLine("\n{blue}Entry Info{reset} {yellow}for{reset} '{green}{entry}{reset}':\n".fmt & "‾".repeat(17+entry.len))
-  stdout.writeLine(" {red}Entry{reset}: {green}{tmp[^1]}{reset}".fmt)
+  stdout.writeLine(" {red}Entry{reset}: {green}{entry}{reset}".fmt)
   stdout.writeLine(" {red}Type{reset}: ".fmt, if info.kind == pcDir: "{blue}Folder{reset}".fmt elif info.kind == pcLinkToDir: "{yellow}Symlink->Folder{reset}".fmt elif info.kind == pcFile: "{blue}File{reset}".fmt else: "{yellow}Symlink->File{reset}".fmt)
   stdout.writeLine(" {red}Permissions{reset}: {green}{str}{reset}".fmt)
   stdout.writeLine(" {red}Size{reset}: {cyan}{size}{reset}".fmt)
