1e2b4faa5edd04e9bfeb68cf9cd68ce93a9f94d4
sneekyfoxx <sneekyfoxx09@gmail.com>
2024-07-27 02:32:53 -0400

    modified install-nim.sh

--- install-nim.sh
+++ install-nim.sh
@@ -2,7 +2,7 @@
 
 install_nim() {
   curl https://nim-lang.org/choosenim/init.sh -sSf | sh;
-  export PATH="${PATH}:${HOME}/.nimble/bin";
+  echo -e '\nexport PATH="${PATH}:${HOME}/.nimble/bin"' >> "${HOME}/.zshrc";
   choosenim stable;
 }
 
